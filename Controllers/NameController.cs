5d9bf41918648d1a1d7b539484057333c55464fc
Mischievous07 <hkravitz715@gmail.com>
2025-11-01 22:10:36 -0400

    New Name Controller

---
+++ Controllers/NameController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class NameController : ControllerBase
+    {
+        // GET api/name
+        [HttpGet]
+        public ActionResult<object> Get()
+        {
+            // Replace "Alice" with any name you want to return
+            var response = new { name = "Holden" };
+
+            // Returns HTTP 200 with JSON: { "name": "Alice" }
+            return Ok(response);
+        }
+
+    }
+}
+
